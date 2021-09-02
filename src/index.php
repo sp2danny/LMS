23b02a1d8e2b9451f608bb6da79648bc8458b679
Daniel Nyström <sp2danny@gmail.com>
2021-09-02 10:40:12 +0200

    update

--- src/index.php
+++ src/index.php
@@ -14,21 +14,46 @@ if ($styr) {
 
 	echo " --- start --- <br><br>";
 
+	$seg = 'segment-' . htmlspecialchars($_GET["seg"]);
+
 	while (true) {
 
-		$buffer = fgets($styr, 4096);
-
+		$buffer = fgets($styr, 4096); // or break;
 		if (!$buffer) break;
+		$buffer = trim($buffer);
+		$len = strlen($buffer);
+		if ($len == 0) continue;
 
-		echo $buffer;
-		echo "<br><br>";
+		if ( ($buffer[0] == '[') && ($buffer[$len-1] == ']') ) {
+			$curr = substr( $buffer, 1, $len-2 );
+			continue;
+		}
+
+		if ($curr == $seg) {
+			$s1 = substr( $buffer, 0, 2 );
+			$s2 = substr( $buffer, 2 );
+			echo $s1 . " , " . $s2 . "<br>";
+			if ($s1 == 't=') {
+				// text
+				echo $s2 . "<br>";
+			if ($s1 == 'e=') {
+				// embed
+			} else if ($s1 == 'q=') {
+				// query
+			} else if ($s1 == 's=') {
+				// submit
+			} else if ($s1 == 'n=') {
+				// next
+			}
+
+		}
 
 	}
 
-	echo " --- stop --- <br><br>";
+	echo "<br> --- stop --- <br>";
 
 } else {
-	echo " --- error --- <br><br>";
+	echo "<br> --- error --- <br>";
 }
 
 fclose($styr);
