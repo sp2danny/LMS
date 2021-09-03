fecd70a3f6bf49b67d3bddb4cf51253c446ed5ed
Daniel Nyström <sp2danny@gmail.com>
2021-09-03 11:04:58 +0200

    embed

--- src/index.php
+++ src/index.php
@@ -6,16 +6,46 @@
 <?php
 
 
+function getparam($key, $def = "")
+{
+	$ok = false;
+	$res = $def;
+
+	try {
+		if (array_key_exists("$key", $_GET)) {
+			$res = $_GET[$key];
+			$ok = true;
+		}
+	} catch(Exception $e) {
+	}
+
+	if (!$ok) try {
+		if (array_key_exists($key, $_POST)) {
+			$res = $_POST[$key];
+			$ok = true;
+		}
+	} catch(Exception $e) {
+	}
+
+	if ($ok)
+	{
+		return $res;
+	} else {
+		return $def;
+	}
+}
+
 
 $styr = fopen("styr.txt", "r") or die("Unable to open file!");
 
 
 if ($styr) {
 
-	//echo " --- start --- <br><br>";
+	$seg = 'segment-' . htmlspecialchars(getparam("seg", "1"));
 
-	$seg = 'segment-' . htmlspecialchars($_GET["seg"]);
+	$qnum = 0;
 
+	$eol = "\n";
 
 	while (true) {
 
@@ -30,7 +60,6 @@ if ($styr) {
 			continue;
 		}
 
-		$qnum = 0;
 
 		if ($curr == $seg) {
 			$s1 = substr( $buffer, 0, 2 );
@@ -39,11 +68,24 @@ if ($styr) {
 
 			if ($s1 == 't=') {
 				// text
-				echo $s2 . "<br>";
+				echo $s2 . "<br>" . $eol;
 			} else if ($s1 == 'f=') {
-				echo '<form action="' . $s2 . '" method="post">';
+				echo '<form action="' . $s2 . '" method="POST">' . $eol;
+				echo '<input type="hidden" value="' . $seg . '" id="seg" name="seg">' . $eol;
+				echo '<input type="hidden" value="' . $s2 . '" id="batteri" name="batteri">' . $eol;
+				echo '<input type="hidden" value="7211064634" id="person" name="person">' . $eol;
 			} else if ($s1 == 'e=') {
 				// embed
+				//echo '<iframe src="https://player.vimeo.com/video/';
+				//echo $s2;
+				//echo '" width="640" height="360" frameborder="1" allow="autoplay; fullscreen" allowfullscreen></iframe>\n';
+
+
+				echo '<div style="padding:75% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/597083498?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&amp;h=3c84b2f529"';
+				echo ' frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:75%;height:75%;" title="out_00.mp4"></iframe></div>';
+				echo '<script src="https://player.vimeo.com/api/player.js"></script>' . $eol;
+
+
 			} else if ($s1 == 'q=') {
 				$qnum++;
 				$valnum = 0;
@@ -58,19 +100,19 @@ if ($styr) {
 						$s2 = '';
 					}
 					if ($valnum == 0) {
-						echo '<h3>' . $s3 . '</h3>';
-						echo '<div class="form-group"><ol>';
+						echo '<h3>' . $s3 . '</h3>' . $eol;
+						echo '<div class="form-group"><ol> ' . $eol;
 					} else {
-						echo '<li> <input type="radio" name="' . $qnum . '" value="' . $valnum . '" />' . $s3 . '</li>';
+						echo '<li> <input type="radio" name="' . $qnum . '" value="' . $valnum . '" />' . $s3 . '</li>' . $eol;
 					}
 					if (!$p) break;
 					$valnum++;
 				}
-				echo '</ol></div>';
+				echo '</ol></div>' . $eol;
 
 			} else if ($s1 == 's=') {
 				// submit
-				echo '<input type="submit" value="' . $s2 . '">';
+				echo '<input type="submit" value="' . $s2 . '">' . $eol;
 			} else if ($s1 == 'n=') {
 				// next
 			}
@@ -82,7 +124,7 @@ if ($styr) {
 	//echo "<br> --- stop --- <br>";
 
 } else {
-	echo "<br> --- error --- <br>";
+	echo "<br> --- error --- <br>\r\n";
 }
 
 fclose($styr);
--- src/score.php
+++ src/score.php
@@ -6,22 +6,49 @@
 
 
 
+function getparam($key, $def = "")
+{
+	$ok = false;
+	$res = $def;
+
+	try {
+		if (array_key_exists("$key", $_GET)) {
+			$res = $_GET[$key];
+			$ok = true;
+		}
+	} catch(Exception $e) {
+	}
+
+	if (!$ok) try {
+		if (array_key_exists($key, $_POST)) {
+			$res = $_POST[$key];
+			$ok = true;
+		}
+	} catch(Exception $e) {
+	}
+
+	if ($ok)
+	{
+		return $res;
+	} else {
+		return $def;
+	}
+}
+
+
+
+
+
+
+echo "seg = " . getparam("seg") . "<br>\n";
+
+echo "batteri = " . getparam("batteri") . "<br>\n";
+
+echo "person = " . getparam("person") . "<br>\n";
 
 
 
 echo "registrerat i databasen<br>";
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
