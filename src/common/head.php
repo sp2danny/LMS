ed605a7679d8c8cba69f9ef6a0a7c113184d486f
Daniel Nyström <sp2danny@gmail.com>
2021-09-29 03:41:43 +0200

    css

--- src/common/head.php
+++ src/common/head.php
@@ -7,9 +7,9 @@
 <?php
 
 if (glob('index.html')) {
-	echo '<link rel="stylesheet" href="common/main.css">' . "\n";
+	echo '<link rel="stylesheet" href="common/main-v01.css">' . "\n";
 } else {
-	echo '<link rel="stylesheet" href="../common/main.css">' . "\n";
+	echo '<link rel="stylesheet" href="../common/main-v01.css">' . "\n";
 	echo '<link rel="stylesheet" href="local.css">' . "\n";
 }
 
