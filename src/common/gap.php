13ae10e778a8871ac28b625ac6bbf5ec2a912b4d
Daniel Nyström <sp2danny@gmail.com>
2022-05-13 00:36:38 +0200

    update

--- src/common/gap.php
+++ src/common/gap.php
@@ -6,7 +6,7 @@
 function gap_query($to, $data, $args)
 {
 
-	$to->regLine("<br> g&ouml;r gapanalys " . $args[0] . "-" . $args[1] . " h&auml;r <br>");
+	$to->regLine("<br> g&ouml;r gapanalys " . $args[0] . "-" . $args[1] . " h&auml;r <br><hr>");
 
 	$gaptxt = fopen("../common/gap-" . $args[0] . ".txt", "r");
 
@@ -23,21 +23,33 @@ function gap_query($to, $data, $args)
 
 	$to->startTag("form", "id='gap' action='../common/gap_post.php'");
 
-	$to->scTag("input", "type='text' id='pnr' name='pnr' value='" . $data->pnr . "'");
-	$to->scTag("input", "type='text' id='gap' name='gap' value='" . $args[1] . "'");
+	$to->scTag("input", "type='hidden' id='pnr' name='pnr' value='" . $data->pnr . "'");
+	$to->scTag("input", "type='hidden' id='gap' name='gap-name' value='" . $args[0] . "'");
+	$to->scTag("input", "type='hidden' id='gap' name='gap-num' value='" . $args[1] . "'");
+	$to->scTag("input", "type='hidden' id='gap' name='gap-cnt' value='" . count($gaplst) . "'");
 
 	$i = 1;
+	$to->startTag("table");
 	foreach ($gaplst as $value) {
+		$to->startTag("tr");
 		$qq = "q".$i;
+		$to->startTag("td");
 		$to->startTag("label", "for='" . $qq . "'");
 		$to->regLine($value);
 		$to->stopTag("label");
-		$to->scTag("input", "type='number' id='".$qq."' name='".$qq."' value='0'");
-		$to->regLine("<br>");
+		$to->stopTag("td");
+		$to->startTag("td");
+		$to->scTag("input", "type='range' min=0 max=100 id='".$qq."' name='".$qq."' value='0'");
+		//$to->regLine("<br>");
+		$to->stopTag("td");
 		++$i;
+		$to->stopTag("tr");
 	}
+	
+	$to->stopTag("table");
+	$to->regLine("<hr>");
 
-	$to->scTag("input", "type='submit' value='submit'");
+	$to->scTag("input", "type='submit' value='Klar'");
 
 	$to->stopTag("form");
 
--- src/common/gap_post.php
+++ src/common/gap_post.php
@@ -1,10 +1,54 @@
 
 <!-- inlude gap_post.php -->
 
+<html>
+<head>
+
 <?php
 
+	include('connect.php');
+	include('common.php');
+	
+	// gap_post.php?pnr=5906195697&gap-name=motiv&gap-num=1&gap-cnt=5&q1=22&q2=54&q3=18&q4=55&q5=31
 
+	$pnr = getparam("pnr", "0");
+	$gapName = getparam('gap-name', "");
+	$gapNum = getparam('gap-num', "");
+	$gapCnt = getparam('gap-cnt', "");
 
+	$query = "SELECT * FROM pers WHERE pnr='" .$pnr . "'";
+
+	$pid = getparam("pid", "0");
+
+	$res = mysqli_query($emperator, $query);
+	if (!$res)
+	{
+		$to->regLine('DB Error');
+	} else {
+		$prow = mysqli_fetch_array($res);
+		if (!$prow) {
+			$to->regLine('DB Error');
+		} else {
+			$pid = $prow['pers_id'];
+		}
+	}
+
+	for ($i=1; $i<=$gapCnt; ++$i)
+	{
+		$qq = 'q' . $i;
+		$par = getparam($qq, 0);
+		
+		$query = "INSERT INTO data ";
+		$query .= "( pers, type, value_a, value_b )" ;
+		$query .= " VALUES ( " . $pid . ',' ;
+		$query .= "7," . $i . ',' . $par . ' )' ;
+
+		if(!mysqli_query( $emperator, $query ))
+		{
+			echo "<br>error<br>";
+			die('Error: ' . mysqli_error($emperator));
+		}
+	}
 
 
 ?>
