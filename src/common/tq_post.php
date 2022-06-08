d5b0f4610d3adea18b1523029c8d0645fc156d1d
Daniel Nyström <sp2danny@gmail.com>
2022-06-08 12:50:33 +0200

    update

--- src/common/tq_post.php
+++ src/common/tq_post.php
@@ -9,7 +9,7 @@
 	include_once('connect.php');
 	include_once('common.php');
 
-	// gap_post.php?pnr=5906195697&gap-name=motiv&gap-num=1&gap-cnt=5&q1=22&q2=54&q3=18&q4=55&q5=31
+	// tq_post.php?pnr=5906195697&tq-11=abc1&tq-12=abc2&tq-13=abc3&tq-14=abc4&tq-15=abc5
 
 	$pnr = getparam("pnr", "0");
 
@@ -23,56 +23,36 @@
 
 	$err = false;
 
-	//
-	//$res = mysqli_query($emperator, $query);
-	//if (!$res)
-	//{;
-	//	$err = 'DB Error, query person --'.$query.'--';
-	//} else {
-	//	$prow = mysqli_fetch_array($res);
-	//	if (!$prow) {
-	//		$err = 'DB Error, fetch person --'.$query.'--';
-	//	} else {
-	//		$pid = $prow['pers_id'];
-	//	}
-	//}
-	//
-	//$query = "INSERT INTO surv ";
-	//$query .= "( pers, type, name, seq )" ;
-	//$query .= " VALUES ( " . $pid . ',' ;
-	//$query .= "7,'" . $gapName . "'," . $gapNum . ' )' ;
-	//
-	//$res = mysqli_query( $emperator, $query );
-	//$sid = 0;
-	//if (!$res)
-	//{
-	//	$err = 'DB Error, query insert surv --'.$query.'--';
-	//} else {
-	//	$sid = $emperator->insert_id;
-	//	//$prow = mysqli_fetch_array($res);
-	//	//if (!$prow) {
-	//	//	$err = 'DB Error, insert surv, fetch result >>'.$query.'<<';
-	//	//} else {
-	//	//	$sid = $prow['surv_id'];
-	//	//}
-	//}
-	//
-	//for ($i=1; $i<=$gapCnt; ++$i)
-	//{
-	//	$qq = 'q' . $i;
-	//	$par = getparam($qq, 0);
-	//	
-	//	$query = "INSERT INTO data ";
-	//	$query .= "( pers, type, value_a, value_b, surv )" ;
-	//	$query .= " VALUES ( " . $pid . ',' ;
-	//	$query .= "7," . $i . ',' . $par . ',' . $sid . ' )' ;
-	//
-	//	if(!mysqli_query( $emperator, $query ))
-	//	{
-	//		$err = 'DB Error, query insert data --'.$query.'--';
-	//	}
-	//}
+	$res = mysqli_query($emperator, $query);
+	if (!$res)
+	{
+		$err = 'DB Error, query person --'.$query.'--';
+	} else {
+		$prow = mysqli_fetch_array($res);
+		if (!$prow) {
+			$err = 'DB Error, fetch person --'.$query.'--';
+		} else {
+			$pid = $prow['pers_id'];
+		}
+	}
 	
+	for ($qi=11; $qi<20; ++$qi) {
+		$str = 'tq-' . $qi;
+		$val = getparam($str, '');
+		if (!empty($val)) {
+			$query = "INSERT INTO data (";
+			$query .= 'pers, type, value_c';
+			$query .= ') VALUES (';
+			$query .= $pid, ', ', $qi, ', ', '"' . $val . '"' . ')';
+			$res = mysqli_query($emperator, $query);
+			if (!$res)
+			{
+				$err = 'DB Error, insert --'.$query.'--';
+				break;
+			}
+		}
+	}
+
 	echo "</head><body>";
 	if ($err === false)
 		echo 'All Ok.<br>';
