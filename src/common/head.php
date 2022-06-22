172d5d0cdd535acedb473c9902e67990f962c9cc
Daniel Nyström <sp2danny@gmail.com>
2022-06-22 17:12:40 +0200

    update

--- src/common/head.php
+++ src/common/head.php
@@ -9,9 +9,11 @@
 
 if (glob('index.html')) {
 	echo '<link rel="stylesheet" href="common/main-v02.css">' . "\n";
+	echo '<link rel="icon" href="common/favicon.ico">' . "\n";
 } else {
 	echo '<link rel="stylesheet" href="../common/main-v02.css">' . "\n";
 	echo '<link rel="stylesheet" href="local.css">' . "\n";
+	echo '<link rel="icon" href="../common/favicon.ico">' . "\n";
 }
 
 ?>
