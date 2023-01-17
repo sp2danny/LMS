3232a36d0143063faa8a9a8b38f1954d9418a246
Daniel Nyström <sp2danny@gmail.com>
2023-01-17 05:01:40 +0100

    update

---
+++ src/survey/survey.php
@@ -0,0 +1,35 @@
+
+
+<!DOCTYPE html>
+
+<html>
+	<head>  
+
+		<title> Kund Undersökning </title>
+
+		<link rel="stylesheet" href="../site/common/main-v03.css">
+		<link rel="icon" href="../site/common/favicon.ico">
+	</head>
+
+	<body>
+		<div>
+			<br /> 
+			<img width=50%  src=" ../site/common/logo.png" /> <br />
+			<div>
+				<br /> <br />
+				stresstratt
+			</div>
+		</div>
+	</body>
+
+</html>
+
+
+
+<?php
+
+
+
+
+?> 
+
