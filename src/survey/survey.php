7aa34ba33f99233f351f06f8bf541ea1a2ded79a
Daniel Nyström <sp2danny@gmail.com>
2023-01-18 13:52:36 +0100

    update

--- src/survey/survey.php
+++ src/survey/survey.php
@@ -7,17 +7,21 @@
 
 		<title> Kund Undersökning </title>
 
-		<link rel="stylesheet" href="../site/common/main-v03.css">
-		<link rel="icon" href="../site/common/favicon.ico">
+		<link rel="stylesheet" href="../site/common/main-v03.css" />
+		<link rel="icon" href="../site/common/favicon.ico" />
 	</head>
 
 	<body>
 		<div>
 			<br /> 
-			<img width=50%  src=" ../site/common/logo.png" /> <br />
+			<img width=50%  src="../site/common/logo.png" /> <br />
 			<div>
 				<br /> <br />
 				stresstratt
+				<canvas>
+				din browser st&ouml;der inte canvas
+				</canvas>
+				
 			</div>
 		</div>
 	</body>
