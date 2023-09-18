6a389aa542e4a232984ad42b4d635cccca217b03
Daniel Nyström <sp2danny@gmail.com>
2023-09-18 14:26:29 +0200

    upd

--- src/klarna/php/dologin.php
+++ src/klarna/php/dologin.php
@@ -3,36 +3,79 @@
 
 	include "getparam.php";
 
-	$hostname_emperator = "mind2excellence.se.mysql";
-	$database_emperator = "mind2excellence_selms"; 
-	$username_emperator = "mind2excellence_selms";
-	$password_emperator = "Gra55bben";
+	run();
 
-	$emperator = mysqli_connect(
-		$hostname_emperator, 
-		$username_emperator, 
-		$password_emperator,
-		$database_emperator
-	);
-
-	$BaseDomain = "mind2excellence.se";
-	
-	if(!$emperator)
+	function run()
 	{
-		echo " DB connect failed <br /> \n ";
-	}
 
-	$pnr = getparam("pnr");
+		$hostname_emperator = "mind2excellence.se.mysql";
+		$database_emperator = "mind2excellence_selms"; 
+		$username_emperator = "mind2excellence_selms";
+		$password_emperator = "Gra55bben";
 
-	$query = "SELECT * FROM pers WHERE pnr='" . $pnr . "'";
+		$emperator = mysqli_connect(
+			$hostname_emperator, 
+			$username_emperator, 
+			$password_emperator,
+			$database_emperator
+		);
 
-	$res = mysqli_query( $emperator, $query );
-	if ($res) {
-		$row = mysqli_fetch_array($res);
-		if ($row) {
-			echo "found one<br>";
+		$BaseDomain = "mind2excellence.se";
+	
+		if(!$emperator)
+		{
+			echo " DB connect failed <br /> \n ";
+			return;
 		}
+
+		$pnr = getparam("pnr");
+
+		$query = "SELECT * FROM pers WHERE pnr='" . $pnr . "'";
+		$pid = 0;
+
+		$res = mysqli_query( $emperator, $query );
+		if ($res) {
+			$row = mysqli_fetch_array($res);
+			if ($row) {
+				$pid = $row['pers_id'];
+			}
+		}
+
+		if (!$pid)
+			$pid = getparam("pid");
+
+		if (!$pid) {
+			echo " personen hittades inte <br /> \n ";
+			return;
+		}
+
+		echo " <hr> <br> \n";
+
+		$query = "SELECT * FROM data WHERE pers='" . $pid . "' AND type='51'";
+		$acc = [];
+
+		$res = mysqli_query( $emperator, $query );
+		if ($res) while ($row = mysqli_fetch_array($res)) {
+			$prod = $row['value_a'];
+			$acc[] = $prod;
+			echo " hittade " . $prod . " <br>\n";
+		}
+
+		echo " <hr> <br> \n";
+
+		foreach ($acc as $val) {
+			$query = "SELECT * FROM prod WHERE prod_id='" . $val . "'";
+			$res = mysqli_query( $emperator, $query );
+			if ($res) if ($row = mysqli_fetch_array($res)) {
+				echo " > " . $row['name'] . " <br>\n";
+			}
+		}
+
+		$res = mysqli_query( $emperator, $query );
+
+
 	}
+
 
 
 ?>
