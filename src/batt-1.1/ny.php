1e74330dc4792cb633e735ae9ea8ffee33f2dcf0
Daniel Nyström <sp2danny@gmail.com>
2023-12-13 14:51:39 +0100

    update

---
+++ src/batt-1.1/ny.php
@@ -0,0 +1,20 @@
+
+<?php
+
+include '../common/head.php';
+include '../common/common.php';
+include '../common/tagOut.php';
+include '../common/connect.php';
+include '../common/ny.php';
+
+$styr = fopen("styr.txt", "r") or die("Unable to open file!");
+$local = "./";
+$common = "../common/";
+
+index($styr, $local, $common);
+
+fclose($styr);
+
+?> 
+
+</html>
