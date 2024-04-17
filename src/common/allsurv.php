033e7c9870baa39b63d707421d236ab0c3720946
Daniel Nyström <sp2danny@gmail.com>
2024-04-17 09:55:52 +0200

    update

--- src/common/allsurv.php
+++ src/common/allsurv.php
@@ -1,8 +1,12 @@
 
+<!DOCTYPE html>
+
 <?php
 
 include_once "../../survey/php/00-common.php";
 include_once "../../survey/php/00-connect.php";
+
+include_once "tagOut.php";
 
 class DataPoint
 {
@@ -10,42 +14,36 @@ class DataPoint
 	public $worst;
 	public $wname;
 	public $when;
+	public $link;
 }
 
-
-function DrawData($dps, $to)
+function DrawData($title, $num, $dps, $to)
 {
 	$n = count($dps);
 
-	$str = "    ['" . $title . "'";
-	for ($i=0; $i<$n; ++$i) {
-		$str .= ", '" . $dps[$i]->wname . "'";
-	}
-	$str .= "],\n";
-
-	$str .= "      ['" . $m . "'";
+	$str = " [ ['#', 'Varde', { role: 'link' } ] ";
 
 	for ($i=0; $i<$n; ++$i) {
-		if ($i > 0)
-			$str .= ", ";
-		$str .= $dps[$i]->worst;
+		$str .= " , [ ";
+		$str .= "'" . ($i+1) . "'";
+		$str .= " , " . $dps[$i]->worst;
+		$str .= " , '" . $dps[$i]->link;
+		$str .= "' ] ";
 	}
-
-	$str .= "]";
+	$str .= " ] ";
 
 	$to->startTag('script');
 	$to->regLine("google.charts.load('current', {'packages':['bar']});");
 	$to->regLine("google.charts.setOnLoadCallback(drawChart_bar_" . $num . ");");
 	$to->regLine("function drawChart_bar_" . $num . "() {");
-	$to->regLine("  var data = google.visualization.arrayToDataTable([");
-	$to->regLine($str);
+	$to->regLine("  var data = google.visualization.arrayToDataTable($str);");
 	$to->regLine('  var options = {');
 	$to->regLine('    title: "' . $title . '",');
 	$to->regLine('    width: 450,');
 	$to->regLine('    legend: { position: "none" },');
 	$to->regLine('    chart: { title: "' . "" . '",'); // $title
 	$to->regLine('             subtitle: "" },');
-	$to->regLine('    bars: "horizontal", // Required for Material Bar Charts.');
+	$to->regLine('    bars: "vertical", // Required for Material Bar Charts.');
 	$to->regLine('    axes: {');
 	$to->regLine('      x: {');
 	$to->regLine('        0: { side: "top", label: "Percentage"} // Top x-axis.');
@@ -54,15 +52,31 @@ function DrawData($dps, $to)
 	$to->regLine('    bar: { groupWidth: "90%" }');
 	$to->regLine('  };');
 	$to->regLine('  var chart = new google.charts.Bar(document.getElementById("bar_chart_' . $num . '"));');
+	$to->regLine('  google.visualization.events.addListener(chart, "select", function (e) {');
+    $to->regLine('          var selection = chart.getSelection();');
+    $to->regLine('              if (selection.length) {');
+    $to->regLine('                  var row = selection[0].row;');
+    $to->regLine('                  let link =data.getValue(row, 2);');
+    $to->regLine('                  location.href = link;');
+    $to->regLine('              }');
+    $to->regLine('      });');
 	$to->regLine('  chart.draw(data, options);');
 	$to->regLine("}");
 	$to->stopTag("script");
 	$to->regLine('<div id="bar_chart_' . $num . '" style="width: 450px; height: 250px"></div>');
 
+	// <div id = "container" style = "width: 550px; height: 400px; margin: 0 auto"> </div>
+
 	return true;
 }
 
-echo "<html><head></head><body>\n";
+echo "<html><head>\n";
+
+echo "<script type='text/javascript' src='https://www.gstatic.com/charts/loader.js'></script>\n";
+
+echo "<script type = 'text/javascript'> google.charts.load('current', {packages: ['corechart']}); </script> \n";
+
+echo "</head><body>\n";
 
 $seqs = [];
 $sids = [];
@@ -123,6 +137,9 @@ function getAndFill($sid, $seq)
 	$dp->vals = $vals;
 	$dp->worst = $w;
 	$dp->wname = get_styr($styr, 'querys', "kat.$wi.name", 1);
+	
+	$lnk = "onesurv.php?pid=$pid&sid=$sid&seq=$seq";
+	$dp->link = $lnk;
 
 	return $dp;
 }
@@ -171,6 +188,12 @@ echo "<hr>\n";
 echo $n . "<br> \n";
 
 echo "<hr>\n";
+
+$to = new tagOut;
+
+//function DrawData($title, $num, $dps, $to)
+
+DrawData("Stress", 1, $dps, $to);
 
 echo "</body></html>\n";
 
