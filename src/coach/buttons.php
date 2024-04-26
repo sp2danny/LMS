77ec8c701a141143ab3ce36ae0f5221f9765f738
Daniel Nyström <sp2danny@gmail.com>
2024-04-26 18:09:58 +0200

    upd

---
+++ src/coach/buttons.php
@@ -0,0 +1,16 @@
+<html>
+<head></head><body>
+
+
+				<br /> <br />
+				&nbsp; &nbsp; &nbsp; <a href='../site/common/create_report.php' > <button> Dina kunder </button> </a> <br /> <br />
+				&nbsp; &nbsp; &nbsp; <a href='../debug.php' > <button> Utbildningen i detaljerad visning </button> </a> <br /> <br />
+				&nbsp; &nbsp; &nbsp; <a href='../site/common/cleanup_db.php' > <button> Tar bort namn i systemet </button> </a> <br /> <br />
+				
+				&nbsp; &nbsp; &nbsp; <a href='make_prod.php' > <button> Skapa Ny Produkt </button> </a> <br /> <br />
+				&nbsp; &nbsp; &nbsp; <a href='list_prod.php' > <button> Lista Produkter </button> </a> <br /> <br />
+				
+				&nbsp; &nbsp; &nbsp; <a href='channel.php' > <button> Manage Channels </button> </a> <br /> <br />
+
+</body>
+</html>
