6130d4bdb538935be6c169fe0c271a620b66abd4
Daniel Nyström <sp2danny@gmail.com>
2024-12-06 20:03:34 +0100

    update

--- src/common/gs_listn.php
+++ src/common/gs_listn.php
@@ -1,4 +1,41 @@
 
+<html>
+<head>
+
+	<title> Skattning Data </title>
+
+<style>
+
+* {
+	font-size: 26px;
+}
+
+p {
+	font-size: 26px;
+	margin-left: 15px;
+}
+
+body {
+	margin-bottom: 75px;
+	margin-left: 15px;
+	margin-right: 260px;
+	font-size: 28px;
+	padding: 0px 10px;
+	background-color: #ffffff;
+}
+
+.sw
+{
+    width: 450px;
+}
+
+</style>
+
+</head>
+
+<body>
+
+
 <?php
 
 include_once "connect.php";
@@ -13,13 +50,13 @@ function collect_grp($pid)
 	
 	$query = "SELECT * FROM surv WHERE type=209 AND pers=$pid";
 	$res = mysqli_query($emperator, $query);
-	if ($res) while($row = mysqli_fetch_array($res))
+	if ($res) while ($row = mysqli_fetch_array($res))
 	{
 		$sid = $row['surv_id'];
 		
 		$query2 = "SELECT * FROM data WHERE type=209 AND pers=$pid AND surv=$sid";
 		$res2 = mysqli_query($emperator, $query2);
-		if ($res2) while($row2 = mysqli_fetch_array($res2))
+		if ($res2) while ($row2 = mysqli_fetch_array($res2))
 		{
 			$nam = $row2['value_c'];
 			$val = $row2['value_a'];
@@ -30,17 +67,73 @@ function collect_grp($pid)
 	return $data;
 }
 
+function minmax($es)
+{
+	$res = [];
+	foreach ($es as $key => $val)
+	{
+		$have = false;
+		$min = $max = 0;
+		foreach ($val as $v)
+		{
+			if (!$have)
+			{
+				$min = $max = $v;
+				$have = true;
+			} else {
+				if ($v > $max) $max = $v;
+				if ($v < $min) $min = $v;
+			}
+		}
+		if ($have) {
+			$res[$key]['max'] = $max;
+			$res[$key]['min'] = $min;
+		}
+	}
+	return $res;
+}
+
+function update_g(&$mmg, $gs)
+{
+	foreach ($gs as $key => $val)
+	{
+		$n = 0;
+		$acc = 0;
+		foreach ($val as $v)
+		{
+			++$n;
+			$acc += $v;
+		}
+		if ($n)
+			$mmg[$key]['grp'] = ($acc/$n);
+	}
+}
+
 function index()
 {
 	$pid = getparam("pid");
 
 	$es = collect_stapel_all($pid);
-
-	var_dump($es);
+	
+	$mmg = minmax($es);
 	
 	$gs = collect_grp($pid);
+	
+	update_g($mmg, $gs);
+	
+	echo "<table>\n";
+	echo "<tr> <th> namn </th> <th> min </th> <th> max </th> <th> grp </th> </tr> \n";
 
-	var_dump($gs);
+	foreach ($mmg as $key => $val)
+	{
+		echo "<tr> <td> " . $key;
+		echo "</td> <td> " . $val['min'];
+		echo "</td> <td> " . $val['max'];
+		echo "</td> <td> " . $val['grp'];
+		echo "</td> </tr> \n";
+	}
+	
+	echo "</table>\n";
 
 }
 
@@ -49,4 +142,5 @@ index();
 
 ?>
 
-
+</body>
+</html>
