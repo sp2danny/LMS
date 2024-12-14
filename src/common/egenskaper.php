992edf41e2f42b590a7765576912f885832dd652
Daniel Nyström <sp2danny@gmail.com>
2024-12-15 00:56:26 +0100

    update

--- src/common/egenskaper.php
+++ src/common/egenskaper.php
@@ -10,9 +10,41 @@ include_once 'common_php.php';
 include_once 'connect.php';
 include_once 'discdisplay.php';
 
+echo <<<EOL
+<style>
+
+.main {
+	/*font-family: Arial; */
+	font-size: 26px;
+}
+
+.sml {
+	font-size: 11px;
+}
+
+.twm {
+	padding-left : 35px;
+}
+
+.lf {
+	width : 280px;
+}
+
+.ls {
+	width : 320px;
+}
+
+.cn {
+	text-align : center;
+	font-size: 22px;
+}
+
+</style>
+EOL;
+
 function index()
 {
-	echo "</head><body>\n";
+	echo "</head><body class='main'>\n";
 	
 	global $emperator;
 	
@@ -46,7 +78,62 @@ function index()
 		echo disc_draw($disc['LR'], $disc['UD']);
 	}
 	
-	echo "</td><td> bla bla </td></tr></table>\n";
+	echo "</td><td> \n";
+	
+	echo "<table><tr><td>";
+	
+echo "<td class='twm' >\n";
+echo "Detta är mina styrkor:\n<ul>\n";
+
+for ($i=1; $i<=5; ++$i)
+{
+	echo "\t<li> " . $i . " &nbsp;&nbsp;&nbsp;";
+	$val = ROD('data', ['pers', 'type', 'value_a'], [$pid, 301, $i], 'value_c', '');
+	echo "<input class='lf' id='st_$i' readonly type='text' value='$val' /> ";
+	echo "</li>\n";
+}
+echo "</ul>\n<br />\n";
+
+	echo "</td><td>";
+
+echo "<td class='twm' >\n";
+echo "Detta är mina svagheter:\n<ul>\n";
+
+for ($i=1; $i<=5; ++$i)
+{
+	echo "\t<li> " . $i . " &nbsp;&nbsp;&nbsp;";
+	$val = ROD('data', ['pers', 'type', 'value_a'], [$pid, 302, $i], 'value_c', '');
+	echo "<input class='lf' id='sv_$i' readonly type='text' value='$val' /> ";
+	echo "</li>\n";
+}
+
+echo "</ul>\n<br />\n";
+
+
+	echo "</td></tr><tr><td>";
+
+echo "<td class='twm' >\n";
+echo "Detta är mina motivatorer:\n<ul>\n";
+
+for ($i=1; $i<=5; ++$i)
+{
+	echo "\t<li> " . $i . " &nbsp;&nbsp;&nbsp;";
+	$val = ROD('data', ['pers', 'type', 'value_a'], [$pid, 303, $i], 'value_c', '');
+	echo "<input class='lf' id='mo_$i' readonly type='text' value='$val' /> ";
+	echo "</li>\n";
+}
+
+echo "</ul>\n<br />\n";
+
+
+	echo "</td><td>";
+
+
+
+	echo "</td></tr></table>";
+
+	
+	echo " </td></tr></table>\n";
 
 
 
