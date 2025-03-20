8080fcb170cd80bb346166360192bc1dc3b14924
Daniel Nyström <sp2danny@gmail.com>
2025-03-20 22:46:59 +0100

    gsk dsk

--- src/common/disk_spara.php
+++ src/common/disk_spara.php
@@ -4,9 +4,26 @@
 include "getparam.php";
 include "debug.php";
 
-$str = str_replace("\n", " ", var_export(array_merge($_GET, $_POST), true));
+function grp_sk(fr, by, id, val)
+{
+  var str = "grp-sk-2.php"
+  str += "?fr=" + fr;
+  str += "&by=" + by;
+  str += "&id=" + id;
+  str += "&val=" + val;
+  fetch(str);
+}
+
+$param = array_merge($_GET, $_POST);
+
+$str = str_replace("\n", " ", var_export($param, true));
 
 debug_log($str);
+
+grp_sk($param['for'], $param['by'], 311, $param['LR']);
+
+grp_sk($param['for'], $param['by'], 312, $param['UD']);
+
 
 
 ?>
