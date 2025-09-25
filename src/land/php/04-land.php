a61e370cd07c485aa274cfaa14ad3e41c76897a9
Daniel Nyström <sp2danny@gmail.com>
2025-09-25 10:02:47 +0200

    upd

--- src/land/php/04-land.php
+++ src/land/php/04-land.php
@@ -63,12 +63,19 @@ $eol = "\n";
 		opacity: 0.5;
 	}
 
+	form {
+		border-style : none;
+		background-color: #fff;
+	}
+
 	.shake_green {
 		animation: shake 1.82s cubic-bezier(.36, .07, .19, .97) both infinite;
 		transform: rotate(0);
 		backface-visibility: hidden;
 		perspective: 1000px;
+		
 		background-color: #66d40e;
+
 		color: white;
 		text-shadow: 0 4px 4px #000;
 		border-radius: 12px;
@@ -112,26 +119,17 @@ $eol = "\n";
       padding-bottom: 10px;
 	}
 	p.preamble {
-		<?php echo "font-family: " . get_styr($styr, "querys", "preamble.font.family", $variant) . ";\n" ?>
-		<?php echo "font-size: " . get_styr($styr, "querys", "preamble.font.size", $variant) . ";\n" ?>
+
 	}
 	p.postamble {
-		<?php echo "font-family: " . get_styr($styr, "querys", "postamble.font.family", $variant) . ";\n" ?>
-		<?php echo "font-size: " . get_styr($styr, "querys", "postamble.font.size", $variant) . ";\n" ?>
+
 	}
 	
   </style>
 
   <script>
 
-    function range_slider_change(idx)
-    {
-      inp = document.getElementById( "q" + idx );
-      lbl = document.getElementById( "l" + idx );
 
-      val = inp.value;
-      lbl.innerHTML = " &nbsp; " + val + " &nbsp; ";
-    }
 
   </script>
 
@@ -142,70 +140,33 @@ $eol = "\n";
 		<br /> 
 		<img width=50% src="../../site/common/logo.png" /> <br />
 		<br /> <br /> 
-	
-		<?php echo "<p class='preamble' > \n" . get_styr($styr, "querys", "preamble.text", $variant) . " </p> \n"; ?>
 
-		<?php echo get_styr($styr, "querys", "instructions", $variant); ?>
-		<br /> <br /> 
-		<br /> <hr />
-		<div>
-			<form id='gap' action='05-result.php' >
-				<?php echo "<input type='hidden' id='lid' name='lid' value='" . getparam('lid') . "' />" . $eol; ?>
-				<?php echo "<input type='hidden' id='pnr' name='pnr' value='" . getparam('pnr') . "' />" . $eol; ?>
-				<?php if ($pid) { echo "<input type='hidden' id='pid' name='pid' value='" . $pid . "' />" . $eol; } ?>
-				<table>
-					<?php
-						$num = get_styr($styr, "querys", "num",  $variant);
-						$qn  = get_styr($styr, "querys", "not",  $variant);
-						$qf  = get_styr($styr, "querys", "full", $variant);
 
-						echo "          <tr>" . $eol;
-						echo "            <td></td>" . $eol;
-						echo "            <td>" . $eol;
-						echo "              $qn" . $eol;
-						echo "            </td>" . $eol;
-						echo "            <td></td> " . $eol;
-						echo "            <td>" . $eol;
-						echo "              $qf" . $eol;
-						echo "            </td>" . $eol;
-						echo "            <td></td> " . $eol;
-						echo "          </tr>" . $eol;
 
-						for ($i = 1; $i <= $num; ++$i) {
-							$qq = 'q' . $i;
-							$rsc = "'range_slider_change($i)'";
-							echo "        <tr>" . $eol;
-							echo "          <td width='375px' >" . $eol;
-							//echo "            <label for='" . $qq . "' >" . $eol;
-							echo "              " . get_styr($styr, "querys", "query.$i.text", $variant) . $eol;
-							//echo "            </label>" . $eol;
-							echo "          </td>" . $eol;
-							echo "          <td width='1px' >" . $eol;
-							//echo "            $qn" . $eol;
-							echo "          </td>" . $eol;
-							echo "          <td width='475px' >" . $eol;
-							echo "            <div  class='range' style='--step:10; --min:0; --max:100'  >" . $eol;
-							echo "              <input type='range' class='inputslider' oninput=".$rsc." onchange=".$rsc." min=0 max=100 step=1 id='".$qq."' name='".$qq."' value='0' /> " . $eol;
-							echo "            </div> " . $eol;
-							echo "          </td> " . $eol;
-							echo "          <td width='45px' > " . $eol;
-							echo "             <div id='l$i' > </div> " . $eol;
-							echo "          </td> " . $eol;
-							echo "          <td width='1px' >" . $eol;
-							//echo "            $qf" . $eol;
-							echo "          </td>" . $eol;
-							echo "        </tr> " . $eol;
-						}
-					?>
-				</table>
-				<hr />
-		
-				<?php echo "<p class='postamble' > \n" . get_styr($styr, "querys", "postamble.text", $variant) . " </p> \n"; ?>
+		<?php
 
-				<input class='shake_green' type='submit' value= <?php echo "'" . get_styr($styr, "querys", "button.text", $variant) . "' />"; ?>
-			</form>
-			<br /> <br />
-		</div>
+
+			$n = get_styr($styr, "land", "num", $variant);
+			for ($i = 1; $i<=$n; ++$i)
+			{
+				$t = get_styr($styr, "land", "text." . $i , $variant);
+				echo $t . "\n";
+				if ($i < $n)
+				{
+					echo "<br /> <hr /> \n";
+				}
+			}
+
+			echo "<a href='https://mind2excellence.se/klarna/php/buy.php?id=4335&prod=3' > \n";
+
+			echo "<button class='shake_green' > " .  get_styr($styr, "land", "knapp" , $variant) . " </button> \n";
+
+			echo "</a> \n";
+
+		?>
+
+
+
 		<br /> <br /> 
 
 	</div>
