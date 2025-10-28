17a15058b20fd272df06b0718bdbbedefd96cd6d
Daniel Nyström <sp2danny@gmail.com>
2025-10-28 02:17:31 +0100

    Update 04b-many.php

--- src/land/php/04b-many.php
+++ src/land/php/04b-many.php
@@ -29,6 +29,65 @@ if ($res) if ($row = mysqli_fetch_array($res)) {
 
 $eol = "\n";
 
+$text = "";
+
+$pid = get_styr($styr, 'prod', 'prod.num', $variant);
+
+$pr_title = '';
+$pr_desc = '';
+$pr_price = 0;
+$pr_img = '';
+$query = "SELECT * FROM prod WHERE prod_id=" . $pid;
+
+$res = mysqli_query( $emperator, $query );
+if ($res) if ($row = mysqli_fetch_array($res)) {
+	$pr_title = $row['name'];
+	$pr_desc  = $row['pdesc'];
+	$pr_price = $row['price'];
+	$pr_img   = $row['image'];
+	$pr_unl   = $row['unlocks'];
+	$pr_mr    = $row['MR'];
+}
+
+debug_log($pr_mr);
+
+$rebate = json_decode($pr_mr);
+
+debug_log(print_r($rebate,true));
+
+$rebatestr = "{";
+foreach ($rebate as $k=>$v)
+{
+	$rebatestr .= "'" . $k . "'" . ":" . $v . ",";
+}
+$rebatestr .= "}";
+
+$subs = explode(",", $pr_unl);
+
+$pr_title_arr = [];
+$pr_desc_arr = [];
+$pr_price_arr = [];
+$pr_img_arr = [];
+
+foreach ($subs as $k=>$v) {
+	$query = "SELECT * FROM prod WHERE prod_id=" . $v;
+	$res = mysqli_query( $emperator, $query );
+	if ($res) if ($row = mysqli_fetch_array($res)) {
+		$pr_title_arr[] = $row['name'];
+		$pr_desc_arr[]  = $row['pdesc'];
+		$pr_price_arr[] = $row['price'];
+		$pr_img_arr[]   = $row['image'];
+	}
+}
+
+$pak_sub_tot = 0;
+$i = 0; $n = count($subs);
+for ($i=0; $i<$n; ++$i) {
+	$pak_sub_tot += $pr_price_arr[$i];
+}
+
+$pak_reb = $pak_sub_tot - $pr_price;
+
 ?>
 
 <!DOCTYPE html>
@@ -46,7 +105,88 @@ $eol = "\n";
 
 	<script>
 
+	function on_update_all() { upd_cnt(); }
 
+	function nice_price(p)
+	{
+		p = Math.floor(p);
+		s = "";
+
+		//sp = "&thinsp;";
+		sp = "&#8239;"; // narrow no-break space
+
+		if (p>=1000000)
+		{
+			t = Math.floor(p/1000000);
+			s += t.toString() + sp;
+			p -= t*1000000;
+		}
+
+		if (p>=1000)
+		{
+			t = Math.floor(p/1000);
+
+			ss = t.toString();
+			if (s.length > 0)
+				while (ss.length < 3)
+					ss = "0" + ss;
+			s += ss + sp;
+			p -= t*1000;
+		}
+
+		if (p>=0)
+		{
+			ss = p.toString();
+			if (s.length > 0)
+				while (ss.length < 3)
+					ss = "0" + ss;
+			s += ss;
+		}
+
+		return s + ":-";
+	}
+
+	function upd_cnt()
+	{
+		arr = <?php echo $rebatestr; ?> ;
+
+		pkp = <?php echo $pr_price; ?> ;
+		pkr = <?php echo $pak_reb; ?> ;
+
+		var qt = document.getElementById("quantity").value;
+
+		var sb = document.getElementById("changeblub");
+
+		//sb.innerHTML = qt.value.toString();
+
+		var rr = 0;
+		var mx = 0;
+
+		for (let key in arr) {
+			k = parseInt(key);
+			v = arr[key];
+			if ( (qt >= k) && (k > mx) )
+			{
+				rr = v;
+				mx = k;
+			}
+		}
+
+		ss = "paket rabatt " + nice_price(pkr) + " per paket <br> \n";
+
+		ss += qt.toString() + " styck med " + rr.toString() + "% rabatt <br> \n";
+
+		var maxp = qt * (pkp+pkr);
+		var spfr = qt * pkp;
+		var sper = spfr * (100-rr)/100;
+
+		ss += "slutpris " + nice_price(sper) + " <br> \n";
+
+		ss += "du sparar totalt " + nice_price(maxp-sper) + " <br> \n";
+
+		sb.innerHTML = ss;
+
+	}
 
 	</script>
 
@@ -56,47 +196,17 @@ $eol = "\n";
 	<div>
 		<br /> 
 		<img width=50% src="../../site/common/logo.png" /> <br />
-		<br /> <br /> 
+		<br /> <br />
+		
+		<hr />
+		<h3> Antal </h3> <br />
+		<label for="quantity"> Antal: </label>
+		<input onchange="upd_cnt()" type="number" id="quantity" name="quantity" min="1" > 
+		<hr />
+		<div id="changeblub"> </div>
+		<hr />
 
 		<?php
-
-			$text = "";
-
-			$pid = get_styr($styr, 'prod', 'prod.num', $variant);
-
-			$pr_title = '';
-			$pr_desc = '';
-			$pr_price = 0;
-			$pr_img = '';
-			$query = "SELECT * FROM prod WHERE prod_id=" . $pid;
-
-			$res = mysqli_query( $emperator, $query );
-			if ($res) if ($row = mysqli_fetch_array($res)) {
-				$pr_title = $row['name'];
-				$pr_desc  = $row['pdesc'];
-				$pr_price = $row['price'];
-				$pr_img   = $row['image'];
-				$pr_unl   = $row['unlocks'];
-				$pr_mr    = $row['MR'];
-			}
-
-			$subs = explode(",", $pr_unl);
-
-			$pr_title_arr = [];
-			$pr_desc_arr = [];
-			$pr_price_arr = [];
-			$pr_img_arr = [];
-
-			foreach ($subs as $k=>$v) {
-				$query = "SELECT * FROM prod WHERE prod_id=" . $v;
-				$res = mysqli_query( $emperator, $query );
-				if ($res) if ($row = mysqli_fetch_array($res)) {
-					$pr_title_arr[] = $row['name'];
-					$pr_desc_arr[]  = $row['pdesc'];
-					$pr_price_arr[] = $row['price'];
-					$pr_img_arr[]   = $row['image'];
-				}
-			}
 
 			$i = 0; $n = count($subs);
 			echo " <br> \n";
@@ -148,10 +258,10 @@ $eol = "\n";
 	</div>
 
 	<div style="display:none" >
-		<img id='circImg' src='../red-circle.png' onload='on_update_2()' /> 
+		<img id='circImg' src='../red-circle.png' onload='on_update_all()' /> 
 	</div>
 	<div style="display:none" >
-		<?php echo "<img id='priceImg' src='../pris.jpg' onload='on_update_3(" . $pr_price . ")' /> \n" ?>
+		<?php echo "<img id='priceImg' src='../pris.jpg' onload='on_update_all()' /> \n" ?>
 	</div>
 
 </body>
