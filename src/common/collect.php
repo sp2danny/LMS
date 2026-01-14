a06042d2903d2fcc49307caa3c36030b4d11db04
Daniel Nyström <sp2danny@gmail.com>
2026-01-14 16:03:19 +0100

    gpsk

--- src/common/collect.php
+++ src/common/collect.php
@@ -28,8 +28,41 @@ function index()
 	$to->scTag('img', 'width=50% src="logo.png"');
 
 	$to->stopTag('div');
+
+
+	// 1 - egenskattning
+
+	$to->regLine('<hr>');
+	$to->regLine('<h1> Egenskattning </h1>');
+
+
+	$vg = ROD('data', ['pers', 'type'], [$pid, 201], 'value_a', false);
+	$to->regLine("Värdegrund : $vg <br> \n");
+
+	$ms = ROD('data', ['pers', 'type'], [$pid, 202], 'value_a', false);
+	$to->regLine("MissionStatement : $ms <br> \n");
+
+	$mot = ROD('data', ['pers', 'type'], [$pid, 302], 'value_a', false);
+	$to->regLine("Motivation : $mot <br> \n");
+
+	$sam = ROD('data', ['pers', 'type'], [$pid, 105], 'value_b', false);
+	$to->regLine("Samarbete : $sam <br> \n");
+
+	$str = ROD('data', ['pers', 'type'], [$pid, 101], 'value_b', false);
+	$to->regLine("Styrkor : $str <br> \n");
+
+	$kom = ROD('data', ['pers', 'type'], [$pid, 103], 'value_b', false);
+	$to->regLine("Kommunikation : $kom <br> \n");
+
+	$mal = ROD('data', ['pers', 'type'], [$pid, 104], 'value_a', false);
+	$to->regLine("Målsättning : $kom <br> \n");
+
+
+
 	$to->stopTag('div');
 	$to->stopTag('body');
+
+
 
 }
 
