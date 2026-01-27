e357d904fc59da893b086d6e6de0995061bbe3cf
Daniel Nyström <sp2danny@gmail.com>
2026-01-27 17:03:48 +0100

    Update debug.php

--- src/common/debug.php
+++ src/common/debug.php
@@ -10,7 +10,12 @@ function arr2str($arr)
 	foreach($arr as $val)
 	{
 		if (!$first) $ret .= ",";
-		$ret .= $val;
+
+		if (is_array($val))
+			$ret .= arr2str($val);
+		else
+			$ret .= $val;
+
 		$first = false;
 	}
 	$ret .= "]";
@@ -21,7 +26,28 @@ function debug_log($str)
 {
 	if (is_array($str))
 		$str = arr2str($str);
-	file_put_contents("../../site/common/debug_logs.txt", date('Y-m-d H:i:s  ||  ') . $str . PHP_EOL, FILE_APPEND | LOCK_EX);
+
+	$out = date('Y-m-d H:i:s  ||  ');
+
+	$fn = '';
+	$ln = '';
+
+	$arr = debug_backtrace();
+	if (array_key_exists(0, $arr)) {
+		$aa = $arr[0];
+		if (array_key_exists('file', $aa))
+			$fn = basename($aa['file']);
+		if (array_key_exists('line', $aa))
+			$ln = $aa['line'];
+	}
+
+	$out .= str_pad($fn, 22) . "  ||  ";
+
+	$out .= str_pad($ln, 4) . "  ||  ";
+
+	$out .= $str;
+
+	file_put_contents("../../site/common/debug_logs.txt", $out . PHP_EOL, FILE_APPEND | LOCK_EX);
 }
 
 ?>
