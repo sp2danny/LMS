b01e33e5c27d8005f5e854944519dc81d55d0030
Daniel Nyström <sp2danny@gmail.com>
2026-02-03 10:06:56 +0100

    Update mockup.php

--- src/common/mockup.php
+++ src/common/mockup.php
@@ -13,6 +13,42 @@ include_once 'util.php';
 
 echo '<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>';
 
+?>
+
+<script>
+
+
+function sp(i)
+{
+	targets = [ 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99 ];
+	targ_s  = [ 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85  ];
+	val_e_1 =   [ 78, 25, 34, 98, 56, 33, 90, 34, 56, 67, 23, 99, 78, 56, 65, 23, 99, 78, 56 ];
+	val_b_1 =   [ 55, 56, 23, 67, 76, 34, 78, 34, 99, 12, 34, 34, 78, 34, 99, 12, 34, 88, 34 ];
+	short_desc_1 = [ 'Värdegrund', 'Mission', 'Utveckling', 'Disc', 'Styrkor' ];  
+
+	val_e_2 =   [ 34, 56, 67, 23, 99, 78, 56, 65, 23, 99, 78, 56 ];
+	val_b_2 =   [ 34, 78, 34, 99, 12, 34, 34, 78, 34, 99, 12, 34, 88, 34 ];
+	short_desc_2 = [ 'bb', 'cc', 'dd', 'ee', 'ff', 'gg' ];  
+
+	switch (i)
+	{
+		case 1:
+			DrawSpider('SpiderCanvas', 5, targets, targ_s, val_e_1, val_b_1, short_desc_1, 'daniel' );
+			break;
+		case 2:
+			DrawSpider('SpiderCanvas', 6, targets, targ_s, val_e_2, val_b_2, short_desc_2, 'blubb' );
+			break;
+	}
+}
+
+
+
+</script>
+
+
+<?php
+
+
 echo "\n</head>\n";
 
 function index()
@@ -39,21 +75,29 @@ function index()
 	$to->startTag('tr');
 
 	// spider
-	$to->startTag('td', 'width=300px');
-	$to->scTag('img', 'width=100% src="spdr.png"');
+	$to->startTag('td', 'width=660px');
+
+	echo '<canvas id="SpiderCanvas" width="550" height="630" style="border:1px solid #000000;">' ;
+	echo ' Din browser st&ouml;der inte canvas </canvas> ' . "\n";
+
+	echo " </td> <td> ";
+
+	global $KAT;
+	$KAT = 'prod' ;
+
 	$to->stopTag('td');
 
 	// btns 1
 	$to->startTag('td', 'width=300px');
-	$to->regLine('<button> btn 1 </button> <br> ');
-	$to->regLine('<button> btn 2 </button> <br> ');
-	$to->regLine('<button> btn 3 </button> <br> ');
+	$to->regLine('<button onclick="sp(1)" > btn 1 </button> <br> ');
+	$to->regLine('<button onclick="sp(2)" > btn 2 </button> <br> ');
+	$to->regLine('<button onclick="sp(3)" > btn 3 </button> <br> ');
 	$to->stopTag('td');
 
 	// btns 2
 	$to->startTag('td', 'width=300px');
-	$to->regLine('<button> btn 1 </button> <br> ');
-	$to->regLine('<button> btn 2 </button> <br> ');
+	$to->regLine('<button onclick="sp(4)" > btn 1 </button> <br> ');
+	$to->regLine('<button onclick="sp(5)" > btn 2 </button> <br> ');
 	$to->stopTag('td');
 
 
