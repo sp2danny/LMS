a8f50845f22d4b4cf55805173021c374b946b7a1
Daniel Nyström <sp2danny@gmail.com>
2026-03-06 15:34:19 +0100

    Update vg.php

--- src/data/vg.php
+++ src/data/vg.php
@@ -1,7 +1,24 @@
 
 <?php
 
+include_once "../common/getparam.php";
+include_once "../common/connect.php";
+
 $data = [];
+
+$pid = getparam('pid', false);
+
+$data['egen'] = [];
+$data['grupp'] = [];
+$data['utv'] = [];
+
+if ($pid === false)
+{
+	echo json_encode($data);	
+	exit;
+}
+
+$query = "SELECT * FROM data WHERE pers=$pid";
 
 $data['egen'] = [66,77,88];
 $data['grupp'] = [65,76,87];
