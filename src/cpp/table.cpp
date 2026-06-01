33908efe6e16e25c761401d0a064e9f97c0c288a
Daniel Nyström <sp2danny@gmail.com>
2026-06-01 08:40:31 +0200

    upd

--- src/cpp/table.cpp
+++ src/cpp/table.cpp
@@ -5,6 +5,8 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <sstream>
+#include <print>
 
 struct Res
 {
@@ -15,6 +17,20 @@ struct Res
     Res(std::string s) : ok(false), msg(s) {}
 };
 
+template <typename Out>
+void split(const std::string& s, char delim, Out result) {
+    std::istringstream iss(s);
+    std::string item;
+    while (std::getline(iss, item, delim)) {
+        *result++ = item;
+    }
+}
+
+std::vector<std::string> split(const std::string& s, char delim) {
+    std::vector<std::string> elems;
+    split(s, delim, std::back_inserter(elems));
+    return elems;
+}
 
 Res Main(std::vector<std::string> arg)
 {
@@ -23,6 +39,42 @@ Res Main(std::vector<std::string> arg)
         return { "Expected parameters" };
     }
 
+    std::ifstream ifs{ arg.front() };
+
+    std::vector<std::string> headers;
+
+    std::string line;
+
+    if (!std::getline(ifs, line))
+        return { "Empty file" };
+
+    headers = split(line, ';');
+
+    std::vector<std::vector<std::string>> data;
+
+    while (std::getline(ifs, line))
+    {
+        data.push_back(split(line, ';'));
+    }
+
+    std::println("<table>");
+    std::println("\t<tr>");
+    for (auto& i : headers)
+    {
+        std::println("\t\t<th> {} </th>", i);
+    }
+    std::println("\t</tr>");
+
+    for (auto& l : data)
+    {
+        std::println("\t<tr>");
+        for (auto& i : l)
+        {
+            std::println("\t\t<td> {} </td>", i);
+        }
+        std::println("\t</tr>");
+    }
+    std::println("</table>");
 
     return {};
 }
